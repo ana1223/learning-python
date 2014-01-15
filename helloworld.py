ef54da4661a08add5b9e9e3a7197ed9012075550
ana1223 <anaclararocha1@gmail.com>
2014-01-15 13:27:52 -0500

    hello fun

---
+++ helloworld.py
@@ -0,0 +1,14 @@
+#learning python commands
+
+print "Hello World!"
+
+print "my name is Steven"
+
+print "Lets count some things"
+
+print "Boys", 25 + 30 / 6
+print "Girls", 100 - 25 * 3 % 4
+
+print "Let's count the class:"
+
+print 3 + 2 + 1 - 5 % 2 - 1 / 4 + 6
